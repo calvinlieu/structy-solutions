48ca364952051dc407493fce9d8055230b04038e
Calvin <calvin.lieu@gmail.com>
2022-11-03 13:10:14 -0700

    anagrams.js

---
+++ anagrams.js
@@ -0,0 +1,31 @@
+const anagrams = (s1, s2) => {
+  const count = {};
+  
+  for (let char of s1) {
+    if (!(char in count)) {
+      count[char] = 0;
+    }
+    count[char] += 1
+  }
+  
+  for (let char of s2) {
+    if (count[char] === undefined) {
+      return false;
+    } else {
+      count[char] -= 1
+    }
+  }
+  
+  for (let char in count) {
+    if (count[char] !== 0) {
+      return false;
+    }
+  }
+  
+  return true;
+};
+
+//time and space complexity: o(n + m) where n and m are the length of the input strings. Space: o(n) since we are only using a hash map.
+
+
+  console.log(anagrams('restful', 'fluster')); // -> true)
--- compress.js
+++ compress.js
@@ -1,1 +1,25 @@
 // Write a function, compress, that takes in a string as an argument. The function should return a compressed version of the string where consecutive occurrences of the same characters are compressed into the number of occurrences followed by the character. Single character occurrences should not be changed.
+
+//space and time complexity: O(n * m), O(1)
+
+const uncompress = (s) => {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    let nums = "0123456789"
+    
+    while (j < s.length) {
+      if (nums.includes(s[j])) {
+        j += 1;
+      } else {
+        const num = Number(s.slice(i, j))
+        for (let count = 0; count < num; count++) {
+          result.push(s[j])
+        }
+        j++;
+        i = j;
+      }
+    }
+    
+    return result.join("")
+  };
