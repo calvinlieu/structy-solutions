ba4d86c27e6f5775cc08aaaa9568d626615fb2a5
Calvin <calvin.lieu@gmail.com>
2022-11-04 13:00:57 -0700

    anagrams time and space

--- anagrams.js
+++ anagrams.js
@@ -1,27 +1,27 @@
 const anagrams = (s1, s2) => {
-  const count = {};
-  
+  let count = {};
+
   for (let char of s1) {
-    if (!(char in count)) {
-      count[char] = 0;
+    if (!(count[char])) {
+      count[char] = 0
     }
     count[char] += 1
   }
-  
+
   for (let char of s2) {
     if (count[char] === undefined) {
-      return false;
+      return false
     } else {
       count[char] -= 1
     }
   }
-  
+
   for (let char in count) {
     if (count[char] !== 0) {
       return false;
     }
   }
-  
+
   return true;
 };
 
