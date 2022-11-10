7c78b6e2a929e8d2316758d8e75fdd246bd42152
Calvin <calvin.lieu@gmail.com>
2022-11-10 14:50:23 -0800

    fiveSort.js

---
+++ fiveSort.js
@@ -0,0 +1,20 @@
+const fiveSort = (nums) => {
+  let i = 0;
+  let j = nums.length - 1;
+
+  while (i < j) {
+    if (nums[j] === 5) {
+      j -= 1;
+    } else if (nums[i] === 5) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      i += 1;
+    } else {
+      i += 1;
+    }
+  }
+
+  return nums;
+};
+
+console.log(fiveSort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]));
+// -> [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5] )
