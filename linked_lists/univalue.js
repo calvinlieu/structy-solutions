2d438faf54845c9f4d222bb6c588ba370d29743a
Calvin <calvin.lieu@gmail.com>
2022-11-21 22:04:25 -0800

    univalue.js

---
+++ linked_lists/univalue.js
@@ -0,0 +1,13 @@
+const isUnivalueList = (head) => {
+  let current = head;
+  while (current !== null) {
+    if (current.val !== head.val) {
+      return false;
+    }
+    current = current.next;
+  }
+
+  return true;
+};
+
+
