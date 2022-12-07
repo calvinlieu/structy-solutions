c78c7b39e2ba7991f6f699d1e49df6b7e2c8c4ff
Calvin <calvin.lieu@gmail.com>
2022-12-06 20:43:24 -0800

    added treesum.js

---
+++ binary_trees/treeSum.js
@@ -0,0 +1,22 @@
+const treeSum = (root) => {
+  if (root === null) {
+    return 0;
+  }
+  let sum = 0;
+
+  let queue = [root];
+
+  while (queue.length > 0) {
+    let node = queue.shift();
+    sum += node.val;
+
+    if (node.left !== null) {
+      queue.push(node.left);
+    }
+    if (node.right !== null) {
+      queue.push(node.right);
+    }
+  }
+
+  return sum;
+};
