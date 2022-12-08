abc2c52f585befa43cb8fa8a4e0a766f50d4fd58
Calvin <calvin.lieu@gmail.com>
2022-12-07 22:37:36 -0800

    added mergeTwoLists.js

---
+++ linked_lists/mergeTwoLists.js
@@ -0,0 +1,22 @@
+var mergeTwoLists = function (list1, list2) {
+  let dummyNode = new ListNode(null);
+  let tail = dummyNode;
+
+  // console.log(newNode)
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      tail.next = list1;
+      list1 = list1.next;
+    } else {
+      tail.next = list2;
+      list2 = list2.next;
+    }
+    tail = tail.next;
+  }
+
+  if (list1 !== null) tail.next = list1;
+  if (list2 !== null) tail.next = list2;
+
+  return dummyNode.next;
+};
