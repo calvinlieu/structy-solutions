8ddc7f005f04a3f8232454eb319b4156e24bd337
Calvin <calvin.lieu@gmail.com>
2022-12-11 15:45:31 -0800

    added Treeminvalue recursive way

--- binary_trees/treeMinValue.js
+++ binary_trees/treeMinValue.js
@@ -1,19 +1,34 @@
+// const treeMinValue = (root) => {
+//   let value = Infinity;
+//   let queue = [root];
+
+//   while (queue.length > 0) {
+//     let node = queue.shift();
+//     if (node.val < value) {
+//       value = node.val;
+//     }
+//     if (node.left !== null) {
+//       queue.push(node.left);
+//     }
+//     if (node.right !== null) {
+//       queue.push(node.right);
+//     }
+//   }
+
+//   return value;
+// };
+
+
+
+//recursive way
 const treeMinValue = (root) => {
-  let value = Infinity;
-  let queue = [root];
+  //we check if the tree is empty to have as a default value.
+  if (root === null) return Infinity;
 
-  while (queue.length > 0) {
-    let node = queue.shift();
-    if (node.val < value) {
-      value = node.val;
-    }
-    if (node.left !== null) {
-      queue.push(node.left);
-    }
-    if (node.right !== null) {
-      queue.push(node.right);
-    }
-  }
+  //we make our recursive calls on both left and right child. these will give us back the smallest value.
+  const leftMin = treeMinValue(root.left);
+  const rightMin = treeMinValue(root.right);
 
-  return value;
+  //we run math.min on all of our values which will return us the smallest value.
+  return Math.min(root.val, leftMin, rightMin);
 };
