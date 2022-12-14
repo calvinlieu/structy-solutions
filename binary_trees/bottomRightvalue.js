e840238d34dbb27d3e4e2d10ffbb3c57e77dd8b2
Calvin <calvin.lieu@gmail.com>
2022-12-13 20:32:17 -0800

    bottomRightvalue.js

---
+++ binary_trees/bottomRightvalue.js
@@ -0,0 +1,17 @@
+const bottomRightValue = (root) => {
+  let queue = [root];
+  let current = null;
+  while (queue.length > 0) {
+    current = queue.shift();
+    if (current.left !== null) {
+      queue.push(current.left);
+    }
+    if (current.right !== null) {
+      queue.push(current.right);
+    }
+  }
+  return current.val;
+};
+
+//since we are just looking for the bottom most right node, we can use BFS in order to go down via a queue.
+//this will enable us to get to the last node by traversing through the tree and returning it at the end.
