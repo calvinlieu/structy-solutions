f522f2a08643df7ed8071691230bb39e78c5d132
Calvin <calvin.lieu@gmail.com>
2022-12-26 12:07:28 -0800

    refactored treeLevels recursion

--- binary_trees/treeLevels.js
+++ binary_trees/treeLevels.js
@@ -20,24 +20,22 @@
 //   return levels;
 // };
 
-
 //recursive
 const treeLevels = (root) => {
-    const levels = [];
-    fillLevels(root, levels, 0);
-    return levels;
+  const levels = [];
+  fillLevels(root, levels, 0);
+  return levels;
+};
+
+const fillLevels = (root, levels, levelNum) => {
+  if (root === null) return;
+
+  if (levels.length === levelNum) {
+    levels.push([root.val]);
+  } else {
+    levels[levelNum].push(root.val);
   }
-  
-  const fillLevels = (root, levels, levelNum) => {
-    if (root === null) return;
-    
-    if (levels.length === levelNum) {
-        levels.push([root.val])
-      } else {
-        levels[levelNum].push(node.val)
-      }
-    
-    fillLevels(root.left, levelsNum + 1)
-    fillLevels(root.right, levelsNum + 1)
-    
-  }
+
+  fillLevels(root.left, levels, levelNum + 1);
+  fillLevels(root.right, levels, levelNum + 1);
+};
