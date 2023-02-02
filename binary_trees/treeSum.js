41064fcc52a1360fe39a9bd184087c8ac654d34f
Calvin <calvin.lieu@gmail.com>
2023-02-02 01:06:32 -0800

    tree sum practice

--- binary_trees/treeSum.js
+++ binary_trees/treeSum.js
@@ -1,22 +1,19 @@
 const treeSum = (root) => {
-  if (root === null) {
-    return 0;
-  }
+  if (root === null) return 0;
   let sum = 0;
-
   let queue = [root];
-
+  
   while (queue.length > 0) {
     let node = queue.shift();
     sum += node.val;
-
+    
     if (node.left !== null) {
-      queue.push(node.left);
+      queue.push(node.left)
     }
     if (node.right !== null) {
-      queue.push(node.right);
+      queue.push(node.right)
     }
   }
-
+  
   return sum;
 };
