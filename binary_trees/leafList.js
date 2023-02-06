72a66e046f7e3d85a919e13dd7fafa1fbb9000ea
Calvin <calvin.lieu@gmail.com>
2023-02-05 16:52:58 -0800

    added leafList dfs

---
+++ binary_trees/leafList.js
@@ -0,0 +1,20 @@
+const leafList = (root) => {
+    // todo
+    if (root === null) return [];
+    
+    const leaves = [];
+    const stack = [ root ];
+    while (stack.length > 0) {
+      const current = stack.pop();
+      if (current.left === null && current.right === null) {
+        leaves.push(current.val)
+      }
+      
+      if (current.right !== null) stack.push(current.right);
+      if (current.left !== null) stack.push(current.left);
+      
+    }
+    
+    return leaves;
+  };
+  
