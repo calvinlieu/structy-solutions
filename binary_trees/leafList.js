183ccf9f79e68ab240932d1d90aaa5a729584719
Calvin <calvin.lieu@gmail.com>
2023-02-05 16:56:00 -0800

    leafList recursions

--- binary_trees/leafList.js
+++ binary_trees/leafList.js
@@ -1,20 +1,35 @@
+// const leafList = (root) => {
+//     // todo
+//     if (root === null) return [];
+
+//     const leaves = [];
+//     const stack = [ root ];
+//     while (stack.length > 0) {
+//       const current = stack.pop();
+//       if (current.left === null && current.right === null) {
+//         leaves.push(current.val)
+//       }
+
+//       if (current.right !== null) stack.push(current.right);
+//       if (current.left !== null) stack.push(current.left);
+
+//     }
+
+//     return leaves;
+//   };
+
 const leafList = (root) => {
-    // todo
-    if (root === null) return [];
-    
     const leaves = [];
-    const stack = [ root ];
-    while (stack.length > 0) {
-      const current = stack.pop();
-      if (current.left === null && current.right === null) {
-        leaves.push(current.val)
-      }
-      
-      if (current.right !== null) stack.push(current.right);
-      if (current.left !== null) stack.push(current.left);
-      
-    }
-    
+    fillLeaves(root, leaves);
     return leaves;
   };
   
+  const fillLeaves = (root, leaves) => {
+    if (root === null) return;
+    if (root.left === null && root.right === null) {
+      leaves.push(root.val)
+    }
+    fillLeaves(root.left, leaves);
+    fillLeaves(root.right, leaves);
+    
+  }
