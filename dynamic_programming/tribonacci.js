a3fd7d5a95b70b9cfc5243820adefafaf26af765
Calvin <calvin.lieu@gmail.com>
2023-02-26 20:15:34 -0800

    added notes to memo in tribonacci

--- dynamic_programming/tribonacci.js
+++ dynamic_programming/tribonacci.js
@@ -1,12 +1,29 @@
-
 //brute force with recursion;
 
-const tribonacci = (n) => {
-  if (n === 0 || n === 1) return 0;
+// const tribonacci = (n) => {
+//   if (n === 0 || n === 1) return 0;
 
+//   if (n === 2) return 1;
+
+//   return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+// };
+
+//dynamic programming using memoization
+//declare a memo object
+const tribonacci = (n, memo = {}) => {
+
+    // check if n is in our memo, if so, return the value of the memo at n
+  if (n in memo) return memo[n];
+
+
+  //same base cases
+  if (n === 0 || n === 1) return 0;
   if (n === 2) return 1;
 
-  return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+  //reassign the current memoization with our recursive calls.
+  memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+
+  //return the memo's value at the nth iteration
+  return memo[n];
+  // todo
 };
-
-
